10420e3e8a009e04ed508c3724ee356f2c0ff038
Seth Jennings <sjenning@redhat.com>
2017-03-07 11:10:55 -0600

    remove HandleError() in getPidsForProcess()

--- pkg/kubelet/cm/container_manager_linux.go
+++ pkg/kubelet/cm/container_manager_linux.go
@@ -35,7 +35,6 @@ import (
 	"github.com/opencontainers/runc/libcontainer/configs"
 	"k8s.io/apimachinery/pkg/api/resource"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
-	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/record"
@@ -633,11 +632,9 @@ func getPidsForProcess(name, pidFile string) ([]int, error) {
 	if len(pidFile) > 0 {
 		if pid, err := getPidFromPidFile(pidFile); err == nil {
 			return []int{pid}, nil
-		} else {
-			// log the error and fall back to pidof
-			runtime.HandleError(err)
 		}
 	}
+	// fall back to pidof
 	return procfs.PidOf(name)
 }
 
